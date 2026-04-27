78de15397a4098f88153b86d65ae559fc3d56fe3
sameerbhagtani <bhagtanisameer@gmail.com>
2026-04-27 10:28:03 +0530

    fix minor bug

--- src/utils/thumbnail.js
+++ src/utils/thumbnail.js
@@ -1,7 +1,7 @@
 import sharp from "sharp";
 import path from "path";
 import fs from "fs";
-import { fileURLToPath, fileURLToPathBuffer } from "url";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const UPLOADS_DIR = path.join(__dirname, "../../uploads");
